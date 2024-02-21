c7c26fdda30e15c4719563cdfcc12b9ed8970218
unknown <ccannard@protonmail.com>
2024-02-20 21:55:20 -0800

    old chanes

--- csd_transform.m
+++ csd_transform.m
@@ -29,21 +29,10 @@ function EEG = csd_transform(EEG, chanlocfile, mcont, smoothl, headrad)
 
 % check that data was not already CSD-transformed
 if strcmp(EEG.ref,'csd-transform') == 1
-    error('These data are already CSD-transformed'); 
+    warning('These data are already CSD-transformed'); 
 end
 
-% Channel locations
-if ~exist('chanlocfile','var') || isempty(chanlocfile)
-%     disp('Please select the channel location file corresponding to your EEG montage. It must be a .ced file.')
-%     [locname, locpath] = uigetfile2({'*.'}, 'Select channel locations file (.ced, .locs, .xyz)'); 
-%     chanlocfile = fullfile(locpath, locname);
-    locPath = fileparts(which('csd_transform.m'));
-    chanlocfile = fullfile(locPath, 'chanlocs_standard_BEM_1005.ced');
-    disp('Using default BEM 1005 channel locations.')
-end
-EEG = pop_chanedit(EEG,'rplurchanloc',1,'lookup',chanlocfile);
-
-% Other default inputs
+% Default head modeling parameters
 if ~exist('mcont','var') || isempty(mcont)
     mcont = 4;
     disp('Using default mcont value = 4'); 
@@ -57,13 +46,24 @@ if ~exist('headrad','var') || isempty(headrad)
     disp('Using default headrad value = 10'); 
 end
 
+% Channel locations
+if ~exist('chanlocfile','var') || isempty(chanlocfile)
+%     disp('Please select the channel location file corresponding to your EEG montage. It must be a .ced, .locs, or .xyz file.')
+%     [locname, locpath] = uigetfile2({'*.'}, 'Select channel locations file (.ced, .locs, .xyz)'); 
+%     chanlocfile = fullfile(locpath, locname);
+    locPath = fileparts(which('csd_transform.m'));
+    chanlocfile = fullfile(locPath, 'standard_1005.ced');
+    disp('Using default boundary element model (BEM) 10-05 electrode coordinates.')
+end
+% EEG = pop_chanedit(EEG,'rplurchanloc',1,'lookup',chanlocfile);
+
 % Convert EEG channel locations into CSD locations
 locExt = chanlocfile(end-2:end);
-disp('Converting EEG channel locations into CSD locations')
+disp('Converting Cartesian XYZ electrode coordinates to CSD locations')
 switch locExt
     case 'xyz'
         [labels,X,Y,Z] = textread(chanlocfile, '%s%f%f%f%*[^\n]','commentstyle','c++');
-%         [ThetaRad,PhiRad] = cart2sph(X,Y,Z);
+        [ThetaRad,PhiRad] = cart2sph(X,Y,Z);
         [ThetaRad,PhiRad] = cart2sph([EEG.chanlocs.X],[EEG.chanlocs.Y],[EEG.chanlocs.Z]);
         theta = ThetaRad * 180 / pi;
         phi = PhiRad * 180 / pi;
@@ -72,19 +72,19 @@ switch locExt
             % disp(sprintf('%8s%10.4f%10.4f%10.4f%22.17f', char(lab(n,:)),X(n),Y(n),Z(n),z));
         end
     case 'ced'
-        [Number,labels,th,radius,X,Y,Z,sph_theta,sph_phi,sph_radius] = textread(chanlocfile, '%n%s%f%f%f%f%f%f%f%f%*[^\n]','headerlines',1);
+        [~,labels,th,radius,X,Y,Z,sph_theta,sph_phi,sph_radius] = textread(chanlocfile, '%n%s%f%f%f%f%f%f%f%f%*[^\n]','headerlines',1);
         theta = sph_theta + 90;     % rotate theta +90 degrees
         if theta > 180              % adjust theta if neccessary
             theta = theta - 360;
         end
         phi = sph_phi;
     case 'locs'
-        [Number,th,radius,labels] = textread(chanlocfile, '%n%f%f%s%*[^\n]');
+        [~,th,radius,labels] = textread(chanlocfile, '%n%f%f%s%*[^\n]');
         theta = -th + 90;
         if theta > 180
             theta = theta - 360;      % adjust theta if neccessary
         end
-        theta = theta;
+        % theta = theta;
         phi = 90 - (radius * 180);
     case 'csd'
         [labels,theta,phi] = textread(chanlocfile, '%s%f%f%*[^\n]','commentstyle','c++');
@@ -95,50 +95,47 @@ switch locExt
             'Then adjust your path to load the .ced file you generated']); return
 end
 
-% Montage structure
+% Electrode labels
+% labels = {EEG.chanlocs.labels};
+
+% Find match electrodes in M
+idx = ~ismissing(lower(labels),lower({EEG.chanlocs.labels}));
+labels(idx) = [];
+theta(idx) = [];
+phi(idx) = [];
+if length(labels)~=length({EEG.chanlocs.labels})
+    error("Labels from chanlocfile and labels from EEG file should have same length")
+end
+
+% Reorganize to match order of EEG data
+% [idx, idx1, idx2] = intersect(labels,{EEG.chanlocs.labels});
 for iChan = 1:EEG.nbchan
-    match = strcmpi(labels, {EEG.chanlocs(iChan).labels});
-    M.lab(iChan) = {EEG.chanlocs(match).labels};
+    match = strcmpi({EEG.chanlocs(iChan).labels},labels);
+    M.lab(iChan) = labels(match);
     M.theta(iChan) = theta(match);
     M.phi(iChan) = phi(match);
 end
-% lab = lab0';
-% theta = theta0';
-% phi = phi0';
-% sph_theta = theta - 90;           % rotate theta -90 degrees
-% if sph_theta < -180
-%   sph_theta = sph_theta + 360;      % adjust theta if neccessary
-% end      
-% sph_phi = phi;
-% th = -sph_theta;
-% radius = (90 - phi) / 180;
-% if strcmp(locExt, 'ced')            % reassign spherical angles for X,Y,Z coordinates
-%     theta = sph_theta; 
-%     phi = sph_phi;
-% end
-% % convert Theta and Phi to radians and Cartesian coordinates for optimal resolution
-% ThetaRad = pi / 180 * theta;             
-% PhiRad = pi / 180 * phi;                 
-% [X,Y,Z] = sph2cart(ThetaRad,PhiRad,1);
-% if strcmp(locExt, 'csd')
-%     for n = 1:EEG.nbchan
-%         z = X(n).^2 + Y(n).^2 + Z(n).^2 - 1; % calculate off sphere surface
-%     end
-% end
 
-% ConvertLocations(chanlocfile,'chanlocs_csd.csd',{EEG.chanlocs.labels}');
-% M = ExtractMontage('chanlocs_csd.csd', {EEG.chanlocs.labels}');
-
-% generate transform matrices
+% Compute Current Source Density (CSD) estimates using the spherical spline
+% surface Laplacian algorithm suggested by Perrin et al. (1989, 1990).
+% Notes: 1) GetGH.m differs from GetCSD.m by removing the first three lines
+%             in the GetCSD.m code because spherical angles are defined through
+%             <M>, and by removing the last line handling the EEG/ERP data
+%          2) In contrast to GetCSD.m, the <m> constant may be optionally
+%             specified as an input parameter (default: m = 4)
+%          3) Using the G and H matrices with data not conforming to the original
+%             EEG montage will not produce valid CSDs
+%          4) The orientation of Theta and Phi is defined in the appendix of
+%             Kayser J, Tenke CE, Clin Neurophysiol 2006;117(2):348-368), and
+%             is not identical to other spherical notifications using the
+%             same spherical angle names (e.g., as in EEGlab)
 [csd_G, csd_H] = GetGH(M, mcont);
-% [csd_G2, csd_H2] = GetGH(M2, mcont);
-% sum(csd_G2(2,:) - csd_G(2,:))
 
 % clean up CSD fields from EEG structure
 EEG = rmfield(EEG,'chaninfo');
 
-% Run CSD-transformation
-csd_data = zeros(size(EEG.data)); %preallocate
+% Apply CSD-transformation / Surface Laplacian
+csd_data = zeros(size(EEG.data)); 
 disp('Running CSD-transformation...')
 csd_data(:,:) = current_source_density(EEG.data(:,:),csd_G,csd_H,smoothl,headrad);
 disp('Completed')
@@ -148,6 +145,4 @@ disp('Completed')
 EEG.ref = 'csd-transform';
 EEG.data = csd_data;
 EEG = eeg_checkset(EEG);
-
-end
 
